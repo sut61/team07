fb36827607ac8e1f54648c0e306fd68d0d640371
JohnR46 <Machine.The@hotmail.com>
2018-12-17 04:59:12 +0700

    add old Entity, drug,type,prescription,staff - close #34

---
+++ Drug-Stroage-System/backend/Drug/Drug/src/main/java/SUT61/Team07/Drug/DrugApplication.java
@@ -0,0 +1,14 @@
+package SUT61.Team07.Drug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugApplication.class, args);
+	}
+
+}
+
---
+++ Drug-Stroage-System/backend/Drug/Drug/src/test/java/SUT61/Team07/Drug/DrugApplicationTests.java
@@ -0,0 +1,17 @@
+package SUT61.Team07.Drug;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrugApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
