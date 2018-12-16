afa2df0837b7e4e917e075883242b0e16e687343
JohnR46 <Machine.The@hotmail.com>
2018-12-17 05:18:03 +0700

    add old Entity Drug,Type,Prescription,Staff - close#34

--- Drug-Stroage-System/backend/Drug/Drug/src/main/java/SUT61/Team07/Drug/Repository/DrugRepository.java
+++
@@ -1,8 +0,0 @@
-package SUT61.Team07.Drug.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import SUT61.Team07.Drug.Entity.Drug;
-
-@RepositoryRestResource
-interface DrugRepository extends JpaRepository<Drug, Long> {
-}
