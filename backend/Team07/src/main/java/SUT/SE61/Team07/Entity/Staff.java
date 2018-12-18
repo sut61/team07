f76d847d7877c22ff30049d2e35e5dca907715f2
piyawadeeypy <piyawadee_pam@hotmail.com>
2018-12-18 19:47:28 +0700

    close #18 -Staff.java

---
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Staff.java
@@ -0,0 +1,21 @@
+package SUT.SE61.Team07;
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Staff {
+    @Id @GeneratedValue
+    private Long staffId;
+    private String staffName;
+    private String customerAddress;
+    private String staffTel;
+    private String staffUserID;
+    private String staffPassword;
+
+}
