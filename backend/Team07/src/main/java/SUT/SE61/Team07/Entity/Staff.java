02ff9853ca06c8e6501a3e05e96251c9f26708f9
JohnR46 <Machine.The@hotmail.com>
2019-01-24 04:49:06 +0700

    fix file error coment git

--- backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Staff.java
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Staff.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package SUT.SE61.Team07.Entity;
 
 import javax.persistence.Entity;
@@ -34,46 +33,5 @@ public class Staff {
        this.staffUser = staffUser;
        this.staffPass = staffPass;
        this.staffPhone = staffPhone;
-
-
    }
-=======
-package SUT.SE61.Team07.Entity;
-
-import javax.persistence.Entity;
-import javax.persistence.*;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import lombok.*;
-
-@Entity
-@Data
-@Table(name = "Staff")
-public class Staff {
-
-    @Id
-    @SequenceGenerator(name = "staff_seq", sequenceName = "staff_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "staff_seq")
-    private Long staffId;
-    private @NonNull String staffName;
-    private @NonNull String staffUser;
-    private @NonNull String staffPass;
-    private @NonNull String staffPhone;
-
-   private Staff(){}
-
-   @ManyToOne(fetch = FetchType.LAZY)   //set festch data เป็น LAZY LAZY = fetch when needed EAGER = fetch immediately
-    @JoinColumn(name= "deptId")      // join colume video_id จาก video กับ entity comment
-    private Department department;
-
-   public Staff(String staffName,String staffUser,String staffPass, String staffPhone){
-       this.staffName  = staffName;
-       this.staffUser = staffUser;
-       this.staffPass = staffPass;
-       this.staffPhone = staffPhone;
-
-
-   }
->>>>>>> 9edd6444efd25d3d4e5e4ab5e3e846b2f799aeec
 }
