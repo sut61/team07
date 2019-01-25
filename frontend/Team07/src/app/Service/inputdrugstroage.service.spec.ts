fd44869b3c31e0eb5c640d1428025e826fe8380d
JohnR46 <Machine.The@hotmail.com>
2019-01-25 10:04:13 +0700

    edit input service

---
+++ frontend/Team07/src/app/Service/inputdrugstroage.service.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from '@angular/core/testing';
+InputdrugstroageService
+
+describe('InputdrugstroageService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: InputdrugstroageService = TestBed.get(InputdrugstroageService);
+    expect(service).toBeTruthy();
+  });
+});
