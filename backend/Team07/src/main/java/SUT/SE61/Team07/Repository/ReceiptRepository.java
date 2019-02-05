90b60ca57d910d883378648ed21ddb3561895c64
apichat003 <apichatkhanngoe54@gmail.com>
2019-02-05 22:20:37 +0700

    add Repository.java -close#83

---
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Repository/ReceiptRepository.java
@@ -0,0 +1,12 @@
+package SUT.SE61.Team07.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import SUT.SE61.Team07.Entity.*;
+
+@RepositoryRestResource
+@CrossOrigin("http://localhost:4200")
+public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+}
