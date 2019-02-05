2b5527c1a4a32700d49dfc573a747939d20e0d47
piyawadeeypy <piyawadee_pam@hotmail.com>
2019-02-06 04:41:40 +0700

    close #63 adit Address

--- backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Address.java
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Address.java
@@ -15,13 +15,24 @@ public class Address {
     @SequenceGenerator(name = "address_seq", sequenceName = "address_seq_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_seq")
     private @NonNull Long addressId;
-    private @NonNull String addressName;
+    private @NonNull String houseno;
+    private @NonNull String villageno;
+    private @NonNull String subdistrict;
+    private @NonNull String district;
+    private @NonNull String province;
+    private @NonNull String postcode;
 
     public Address() {
     }
 
-    public Address(String addressName) {
-        this.addressName = addressName;
+    public Address(String houseno,String villageno,String subdistrict,String district,String province,String postcode) {
+        this.houseno = houseno;
+        this.villageno = villageno;
+        this.subdistrict = subdistrict;
+        this.district = district;
+        this.province = province;
+        this.postcode = postcode;
+
 
     }
    
