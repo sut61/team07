39672fbb80d5b15bd2fa5b366d053e023c236174
tuntika18 <tuntika18chin@gmail.com>
2019-02-06 20:54:27 +0700

    close #87 add table notification UI

--- frontend/Team07/src/app/notification/notification.component.ts
+++ frontend/Team07/src/app/notification/notification.component.ts
@@ -4,7 +4,6 @@ import { Router } from "@angular/router";
 import { ActivatedRoute } from "@angular/router";
 
 // import service 
-import { RegisterService } from '../Service/register.service';  // get initial gender bloodtype all
 import { DrugService } from '../Service/drug.service';
 import { NotificationService } from '../Service/notification.service';
 
@@ -14,41 +13,62 @@ import { NotificationService } from '../Service/notification.service';
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
-   // get data from db  
-   category: Array<any>;
-   categoryselect = '';
-   Iinitials: Array<any>;
-   Initialselect = '';
-   genders: Array<any>;
-   genderselect = '';
-   bloodtypes: Array<any>;
-   bloodtypeselect = '';
- 
-   customer = {
-     customerId: Number, customerName: String, customerPhonenumber: String,
-     customerAddress: String, customerUserID: String, customerPassword: String,
-     initial: { initialId: Number, name: String },
-     gender: { genderId: Number, sex: String },
-     bloodType: { bloodTypeId: Number, name: Number }
-   };  // customer by name 
-   //customerselect = '';
- 
-   data: any = {}
- 
+  // get data from db  
+  drug: Array<any>;
+  drugselect = '';
+  notification :Array<any>;
+  timeEat :Array<any>;
+  timeEatselect = '';
 
-  constructor(private route: ActivatedRoute, private notificationservice: NotificationService, private registerservice: RegisterService, private router: Router, private httpClient: HttpClient) { }
 
+
+
+  customer = {
+    customerId: Number, customerName: String, customerPhonenumber: String,
+    customerAddress: String, customerUserID: String, customerPassword: String,
+    initial: { initialId: Number, name: String },
+    gender: { genderId: Number, sex: String },
+    bloodType: { bloodTypeId: Number, name: Number }
+  };  // customer by name 
+  //customerselect = '';
+
+  data: any = {}
+
+
+  constructor(private route: ActivatedRoute, private drugService: DrugService, private notificationservice: NotificationService, private router: Router, private httpClient: HttpClient) { }
+  displayedColumns: string[] = ['position', 'customername', 'drugname', 'timeEat', 'Notification'];
+  
   SaveNotification() {
     // console.log(this.data.symptom)
     // console.log(this.categoryselect)
 
-    this.notificationservice.postNotification(String(this.data.symptom), Number(this.categoryselect), Number(1), Number(this.customer.customerId)).subscribe(data => {
-      console.log(data)
+    // this.notificationservice.postNotification(String(this.data.symptom), Number(this.categoryselect), Number(1), Number(this.customer.customerId)).subscribe(data => {
+    //   console.log(data)
+    // })
 
 
-    })
+
+  }
+
+  showdb() {
+    console.log(this.data.namecus)
+    console.log(this.drugselect)
+    console.log(this.timeEatselect)
+    console.log(this.data.timestartEat)
+  }
 
 
+
+  searchCustomername() {
+    //  console.log(this.data.namecus)
+    if (this.data.namecus === undefined || this.data.namecus == "") {
+      alert("กรุณาใส่ชื่อ")
+    } else {
+      console.log(this.data.namecus)
+      // this.redcorduseservice.getCustomerByname(String(this.data.namecus)).subscribe(data => {
+      //   this.customer = data;
+      // })
+    }
 
   }
 
@@ -57,9 +77,23 @@ export class NotificationComponent implements OnInit {
       this.customer = data;
       console.log(this.customer)
     })
+    this.drugService.getDrug().subscribe(data => {
+      this.drug = data;
+      console.log(this.drug)
+    })
 
-   
-    
+    this.notificationservice.getNotificationAll().subscribe(data=>{
+      this.notification = data;
+      console.log(this.notification)
+    }) 
+
+    this.notificationservice.getTimeEatAll().subscribe(data=>{
+      this.timeEat = data;
+      console.log(this.timeEat)
+    })
+
+
+
   }
 
 
