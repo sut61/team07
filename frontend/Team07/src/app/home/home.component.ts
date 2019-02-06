069209a57448a8dc40eab068c390ea1757dc8bce
JohnR46 <Machine.The@hotmail.com>
2019-02-06 11:45:17 +0700

    send user login to home

--- frontend/Team07/src/app/home/home.component.ts
+++ frontend/Team07/src/app/home/home.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
-
-
+import {ActivatedRoute} from "@angular/router";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+data:any ={}
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(prams=>{
+      this.data = prams
+      console.log(prams)
+    })
   }
 
 }
