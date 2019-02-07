ef461122bf865b129924b318ea21476e928af9a3
piyawadeeypy <piyawadee_pam@hotmail.com>
2019-02-07 11:59:23 +0700

    close #63 Edit UI can save

--- frontend/Team07/src/app/delivery/delivery.component.ts
+++ frontend/Team07/src/app/delivery/delivery.component.ts
@@ -2,29 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { DeliveryService } from '../Service/delivery.service';
 import { HttpClient } from '@angular/common/http';
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-delivery',
   templateUrl: './delivery.component.html',
   styleUrls: ['./delivery.component.css']
 
-  
+
 })
 export class DeliveryComponent implements OnInit {
   staffs: Array<any>;
   customers: Array<any>;
   drugs: Array<any>;
   addresss: Array<any>;
-   addressselect ='';
+  addressselect = '';
 
   detail: any = {
-    staff:'',
-    customer:'',
-    drug:'',
-    amount:'',
+    staff: '',
+    customer: '',
+    drug: '',
+    amount: '',
     netamount: '',
     //address: '',
 
@@ -34,19 +34,19 @@ export class DeliveryComponent implements OnInit {
   emp: any = {
     staffSelect: '',
     customerSelect: '',
-   // addressSelect: '',
+    // addressSelect: '',
     drugSelect: '',
-    
+
   };
 
   // showdatainput() {
   //   console.log("initialId = " + this.initialselect)
   //   console.log("genderselect = " + this.genderselect)
   //   console.log("bloodTypeselect = " + this.bloodTypeselect)
-    
-    
+
+
   // }
-  constructor(private router:Router,private deliveryService: DeliveryService, private httpClient: HttpClient) { }
+  constructor(private router: Router, private deliveryService: DeliveryService, private httpClient: HttpClient) { }
 
   ngOnInit() {
     this.deliveryService.getStaffs().subscribe(data => {
@@ -54,8 +54,8 @@ export class DeliveryComponent implements OnInit {
       console.log(this.staffs);
     });
 
-    
-  
+
+
     this.deliveryService.getCustomers().subscribe(data => {
       this.customers = data;
       console.log(this.customers);
@@ -65,34 +65,34 @@ export class DeliveryComponent implements OnInit {
       console.log(this.addresss);
     });
     this.deliveryService.getDrugs().subscribe(data => {
-    this.drugs = data;
-    console.log(this.drugs);
-     });
+      this.drugs = data;
+      console.log(this.drugs);
+    });
   }
 
-  SubmitdData(){
+  SubmitdData() {
     console.log(this.detail)
-    const data  = this.detail
-      this.router.navigate(['delivery-show',{
+    const data = this.detail
+    this.router.navigate(['delivery-show', {
 
-        staff:        data.staff,
-        customer:     data.customer,
-        address:     data.address,
+      staff: data.staff,
+      customer: data.customer,
+      address: data.address,
 
-        drug:        data.drug,
-        amount:        data.amount,
-        netamount:        data.netamount,
-     
-        }])
- 
+      drug: data.drug,
+      amount: data.amount,
+      netamount: data.netamount,
+
+    }])
+
   }
   save() {
-    this.httpClient.post('http://localhost:8080/Invoice-insert/staff/' + this.emp.staffSelect + '/customer/' + this.emp.customerSelect +'/address/' + this.emp.addressSelect + '/drug/' + this.emp.drugSelect+'/invoice/'+this.detail.amount+'/invoice/'+this.detail.netamount  ,this.emp)
-    .subscribe(
-      data =>   {console.log('PUT Request is successful', data);},
-      error =>  {console.log('Error', error);}
+    this.httpClient.post('http://localhost:8080/Invoice-insert/StaffId/' + this.emp.staffSelect + '/customerId/' + this.emp.customerSelect + '/addressId/' + this.addressselect + '/drugId/' + this.emp.drugSelect + '/Amount/' + String(this.detail.netamount) + '/Netamount/' + String(this.detail.netamount), this.emp)
+      .subscribe(
+        data => { console.log('PUT Request is successful', data); },
+        error => { console.log('Error', error); }
       );
-    }
+  }
 }
 
 
