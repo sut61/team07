9b9612f8e84c72ce2eea0c3b0b52f25974bc04a8
WirayutKh <wirayutkhotsuk@gmail.com>
2019-02-09 22:00:24 +0700

    fix bug in redcorduse

--- backend/Team07/src/main/java/SUT/SE61/Team07/Controller/ResistanceController.java
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Controller/ResistanceController.java
@@ -22,28 +22,15 @@ import SUT.SE61.Team07.Entity.*;
 class ResistanceController {
     private ResistanceRepository resistanceRepository;
     private RecordDrugUseRepository recordDrugUserepository;
-   /* private CustomerRepository customerrepository;
-    private BloodTypeRepository bloodTypeRepository;
-    private DrugRepository drugRepository;
-    private DrugdataRepository drugdatarepository;
-    private StaffRepository staffrepository;*/
 
-    public ResistanceController( ResistanceRepository resistanceRepository,
-                                    RecordDrugUseRepository recordDrugUserepository
-                                    /*CustomerRepository customerrepository,
-                                    BloodTypeRepository bloodTypeRepository,
-                                    DrugRepository drugRepository,
-                                    DrugdataRepository drugdatarepository, 
-                                    StaffRepository staffrepository*/
-                                   ) {
+    public ResistanceController(ResistanceRepository resistanceRepository,
+            RecordDrugUseRepository recordDrugUserepository
+
+    ) {
 
         this.resistanceRepository = resistanceRepository;
         this.recordDrugUserepository = recordDrugUserepository;
-        /*this.customerrepository = customerrepository;
-        this.bloodTypeRepository = bloodTypeRepository;
-        this.drugRepository = drugRepository;
-        this.drugdatarepository = drugdatarepository;
-        this.staffrepository = staffrepository;*/
+
     }
 
     @GetMapping("/Resistance-list")
@@ -51,25 +38,15 @@ class ResistanceController {
         return resistanceRepository.findAll().stream().collect(Collectors.toList());
     }
 
-    @PostMapping("Resistance/{Resistance}/RecordDrugUseId/{RecordDrugUseId}/customerId/{customerId}/bloodTypeId/{bloodTypeId}/drugId/{drugId}/drugdataId/{drugdataId}/symptom/{symptom}/resist/{resist}/staffId/{staffId}")
-    public ResponseEntity<Map<String, Object>> RedcordSumbit(   @PathVariable("RecordDrugUseId") Long recorddrugId,
-                                                                /*@PathVariable("symptom") String symptom,
-                                                                @PathVariable("drugdataId") Long drugdataId, 
-                                                                @PathVariable("staffId") Long staffId,
-                                                                @PathVariable("customerId") Long customerId,*/
-                                                                @PathVariable("resist") String resist) {
+    @PostMapping("Resistance-Insert/RecordDrugUseId/{RecordDrugUseId}/result/{result}")
+    public ResponseEntity<Map<String, Object>> RedcordSumbit(@PathVariable("RecordDrugUseId") Long RecordDrugUseId,
+            @PathVariable("result") String result) {
         try {
-            RecordDrugUse Rdu = this.recordDrugUserepository.findByRecordDrugUseId(recorddrugId);
-            /*Drugdata        Dd  = this.drugdatarepository.findByDrugdataId(drugdataId);
-            Staff           Ss  = this.staffrepository.findByStaffId(staffId);
-            Customer        Cc  = this.customerrepository.findByCustomerId(customerId);*/
-
-            this.resistanceRepository.save(new Resistance(Rdu,resist));
-
+            RecordDrugUse Rdu = this.recordDrugUserepository.findByRecordDrugUseId(RecordDrugUseId);
+            this.resistanceRepository.save(new Resistance(Rdu, result));
             Map<String, Object> json = new HashMap<String, Object>();
             json.put("success", true);
             json.put("status", "save");
-
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Type", "application/json; charset=UTF-8");
             headers.add("X-Fsl-Location", "/");
@@ -80,7 +57,6 @@ class ResistanceController {
             System.out.println("Error Save CancelReservation");
             json.put("success", false);
             json.put("status", "save-false");
-
             HttpHeaders headers = new HttpHeaders();
             headers.add("Content-Type", "application/json; charset=UTF-8");
             headers.add("X-Fsl-Location", "/");
