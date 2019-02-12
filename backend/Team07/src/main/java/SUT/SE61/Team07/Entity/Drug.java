812c0f3888a9aafd85732d09844a3b3b23e1f03c
johnR46 <Machine.The@hotmail.com>
2019-02-13 03:23:17 +0700

    tesst Drug

--- backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Drug.java
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Entity/Drug.java
@@ -1,9 +1,12 @@
 package SUT.SE61.Team07.Entity;
+
 import javax.persistence.Entity;
 import javax.persistence.*;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.*;
@@ -12,25 +15,35 @@ import lombok.*;
 @Data
 public class Drug {
 
-     @Id
+    @Id
     @SequenceGenerator(name = "drug_seq", sequenceName = "drug_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "drug_seq")
-    private  Long drugId;
+    private Long drugId;
 
-    //@Size(min = 3, max = 20)
-    private @NonNull String name;
-    private @NonNull String price;
-    
-    private @NonNull String qty;
-    
+    @NotNull(message = "not Drugname to null")
+    @Pattern(regexp = "[A-Za-z]{3,25}")
+    @Size(min = 3, max = 25)
+    @Column(unique = true)
+    private String name;
 
+    @NotNull(message = "not price  to null")
+    @Pattern(regexp = "[0-9]{1,8}")
+    @Size(min = 1, max = 25)
+    private String price;
 
-   private Drug(){}
-   public Drug(String name,String price,String qty){
-       this.name  = name;
-       this.price = price;
-       this.qty = qty;
-       
-   }
-    
+    @NotNull(message = "not qty  to null")
+    @Pattern(regexp = "[0-9]{1,8}")
+    @Size(min = 1, max = 25)
+    private String qty;
+
+    public Drug() {
+    }
+
+    public Drug(String name, String price, String qty) {
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+
+    }
+
 }
