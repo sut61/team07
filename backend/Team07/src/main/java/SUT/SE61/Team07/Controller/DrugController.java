b523e35a02e4fb3044233d2755d192ac8a322a0f
johnR46 <Machine.The@hotmail.com>
2019-02-15 06:36:24 +0700

    close #124 add update in DrugController

--- backend/Team07/src/main/java/SUT/SE61/Team07/Controller/DrugController.java
+++ backend/Team07/src/main/java/SUT/SE61/Team07/Controller/DrugController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import SUT.SE61.Team07.Entity.*;
 import SUT.SE61.Team07.Repository.*;
@@ -30,42 +33,24 @@ class DrugController {
     public Collection<Drug> drugs() {
         return drugrepository.findAll().stream().collect(Collectors.toList());
     }
+
     @GetMapping("/Drug/{drugId}")
     public Drug drugFind(@PathVariable("drugId") Long id) {
         return drugrepository.findByDrugId(id);
     }
 
+    @PutMapping("/Drug-Update/{drugId}")
+    public ResponseEntity<Object> updateStudent(@RequestBody Drug drug, @PathVariable long drugId) {
+        Optional<Drug> drugOptional = this.drugrepository.findById(drugId);
+
+        if (!drugOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+            drug.setDrugId(drugId);
+
+            drugrepository.save(drug);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
-  // ไว้เพิ่มยาเข้าคลัง 
-// @PostMapping("/Drug-insert/{id}/name/{name}")
-// public ResponseEntity<Map<String, Object>> Drugsumbit(@PathVariable("id")
-// Long id,
-// @PathVariable("name") String name) {
-// try{
-// Drug d = new Drug(name);
-// this.drugrepository.save(d);
-// Map<String, Object> json = new HashMap<String, Object>();
-// json.put("success", true);
-// json.put("status", "save");
-
-// HttpHeaders headers = new HttpHeaders();
-// headers.add("Content-Type", "application/json; charset=UTF-8");
-// headers.add("X-Fsl-Location", "/");
-// headers.add("X-Fsl-Response-Code", "302");
-// return (new ResponseEntity<Map<String, Object>>(json, headers,
-// HttpStatus.OK));
-// }catch (NullPointerException e) {
-// Map<String, Object> json = new HashMap<String, Object>();
-// System.out.println("Error Save CancelReservation");
-// json.put("success", false);
-// json.put("status", "save-false");
-
-// HttpHeaders headers = new HttpHeaders();
-// headers.add("Content-Type", "application/json; charset=UTF-8");
-// headers.add("X-Fsl-Location", "/");
-// headers.add("X-Fsl-Response-Code", "500");
-// return (new ResponseEntity<Map<String, Object>>(json, headers,
-// HttpStatus.INTERNAL_SERVER_ERROR));
-
-// }
-// }
